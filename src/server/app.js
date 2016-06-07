1ad000d596a5f5a5421eae7a88e9a61bf6a34f82
David Sudia <dsudia@gmail.com>
2016-06-07 12:01:24 -0600

    deleted other unecessary files

--- src/server/app.js
+++ src/server/app.js
@@ -1,14 +1,14 @@
 // *** main dependencies *** //
-if ( !process.env.NODE_ENV ) { require('dotenv').config(); }
+if (!process.env.NODE_ENV) {
+    require('dotenv').config();
+}
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var flash = require('connect-flash');
 var session = require('express-session');
-var passport = require('./lib/passport');
 
 
 // *** routes *** //
@@ -22,17 +22,16 @@ var app = express();
 // *** config middleware *** //
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(session({
-  secret: process.env.SECRET_KEY || 'change_me',
-  resave: false,
-  saveUninitialized: true
+    secret: process.env.SECRET_KEY || 'change_me',
+    resave: false,
+    saveUninitialized: true
 }));
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
 
 
 // *** main routes *** //
@@ -41,9 +40,9 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 
@@ -52,23 +51,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.json({
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: {}
+    });
 });
 
 
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('../lib/passport');
-var helpers = require('../lib/helpers');
 
 router.get('/', function(req, res, next) {
   res.sendFile('/src/client/index.html');
