75a6f371e16058dd4df1d5caf502627e49c51a0b
David Sudia <dsudia@gmail.com>
2016-06-07 12:09:41 -0600

    removed call to .env

--- src/server/app.js
+++ src/server/app.js
@@ -1,7 +1,4 @@
 // *** main dependencies *** //
-if (!process.env.NODE_ENV) {
-    require('dotenv').config();
-}
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
