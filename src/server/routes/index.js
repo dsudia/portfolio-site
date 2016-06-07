4a407812da9464b0a71afbd40362f8e72fc3b674
David Sudia <dsudia@gmail.com>
2016-06-07 12:06:03 -0600

    changed port to 80

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.sendFile('/src/client/index.html');
+    res.sendFile('/src/client/index.html');
 });
 
 module.exports = router;
